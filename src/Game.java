020b89100f0c937b3dbafc8ab7e4a4a147f6d124
Bethany Cho <102619753+jellyfishyishy@users.noreply.github.com>
2023-03-22 10:31:51 -0400

    Add files via upload

---
+++ src/Game.java
@@ -0,0 +1,15 @@
+public class Game {
+	NonogramGrid currentSolution;
+	NonogramGrid playerGrid;
+	int mode;
+	
+	public void setMode(int m) {
+		mode = m;
+	}
+	
+	public int getMode() {
+		return mode;
+	}
+	
+
+}
